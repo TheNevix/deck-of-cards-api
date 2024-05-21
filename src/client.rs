c581f984c0f937661d09a2ae80e5cae68ad7fc55
TheNevix <ducelliersaimen@gmail.com>
2024-05-21 20:01:55 +0200

    Added a way to draw cards for a deck.

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,5 @@
 use reqwest::Error;
-use crate::models::Deck;
+use crate::models::{Deck, DrawCardsResponse};
 
 pub struct DeckOfCardsClient {
     base_url: String,
@@ -16,16 +16,25 @@ impl DeckOfCardsClient {
         }
     }
 
+    /// Sets the deck count to the number that has been passed.
+    /// 
+    /// # Parameters
+    /// - `count`: Amount of decks.
     pub fn deck_count(mut self, count: u32) -> Self {
         self.deck_count = count;
         self
     }
 
+    /// Sets if the decks need to be shuffled
+    /// 
+    /// # Parameters
+    /// - `shuffle`: Set shuffle to true or false.
     pub fn shuffle(mut self, shuffle: bool) -> Self {
         self.shuffle = shuffle;
         self
     }
 
+    /// Creates a new deck.
     pub async fn create_deck(&self) -> Result<Deck, Error> {
         let url = format!(
             "{}/new/{}?deck_count={}",
@@ -37,4 +46,25 @@ impl DeckOfCardsClient {
         let deck: Deck = response.json().await?;
         Ok(deck)
     }
+
+    /// Draws a certain amount of cards from a deck
+    /// 
+    /// # Parameters
+    /// - `amount`: Amount of cards to draw.
+    /// - `deck_id`: The id of the deck.
+    pub async fn draw_cards(&self, amount: i16, deck_id: String) -> Result<DrawCardsResponse, Error> {
+        let url = format!(
+            "{}/{}/draw/?count={}",
+            self.base_url,
+            deck_id,
+            amount
+        );
+        let response = reqwest::get(&url).await?;
+        let draw_card_response: DrawCardsResponse = response.json().await?;
+        Ok(draw_card_response)
+    }
+
+
+
+
 }
--- src/models.rs
+++ src/models.rs
@@ -7,3 +7,26 @@ pub struct Deck {
     pub shuffled: bool,
     pub remaining: u32,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DrawCardsResponse {
+    pub success: bool,
+    pub deck_id: String,
+    pub cards: Vec<Card>,
+    pub remaining: i32
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Card {
+    pub code: String,
+    pub image: String,
+    pub images: Images,
+    pub value: String,
+    pub suit: String
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Images {
+    pub svg: String,
+    pub png: String
+}
